929045c9dde7d48453b7fb165b156aed6355732e
AugustoReis82 <79484649+AugustoReis82@users.noreply.github.com>
2022-05-03 20:39:43 -0300

    Inserindo validador de password

--- src/components/sign-up/sign-up.tsx
+++ src/components/sign-up/sign-up.tsx
@@ -194,3 +194,28 @@ export default function SignUp() {
     </ThemeProvider>
   );
 }
+
+/* ### Valdador de Password #####
+function validar(){
+  var senha = formData.senha.value;
+  var rep_senha = formData.rep_senha.value;
+
+  if(senha == "" || senha.length <= 5){
+    alert('preencha o campo senha com no mínimo 6 caracteres');
+    formData.senha.focus();
+    return false;
+  }
+
+  if(rep_senha == "" || rep_senha.length <= 5){
+    alert('preencha o campo senha com no mínimo 6 caracteres');
+    formData.rep_senha.focus();
+    return false;
+  }
+  
+  if (senha != rep_senha) {
+    alert('As senhas devem ser iguais!');
+    form1.senha.focus();
+    return false;
+  }
+
+} */
